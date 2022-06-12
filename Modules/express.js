bda5a1cb0ee90594c5f62ca2938db9452080aa1f
BrunodevBandeira <brunodev29ab@gmail.com>
2022-06-12 15:33:45 -0300

    Middleaware

--- Modules/express.js
+++ Modules/express.js
@@ -19,7 +19,18 @@ app.get("/users", async (req, res) => {
    }
 });
 
-//app.get("/users", async (req, res))
+app.get("/users/:id", async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        return res.status(200).json(user);
+
+    } catch(error) {
+         return res.status(500).send(error.message)
+    }
+ });
+
 
 
 app.post("/users", async (req, res) => {
@@ -30,6 +41,28 @@ app.post("/users", async (req, res) => {
         res.status(500).send(error.message);
     }
 })
+
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(users);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+})
+
+
 
 const port = 8080;
 app.listen(port, () => console.log(`Rodando com express na porta ${port}!`));
--- Modules/expressJson.js
+++ Modules/expressJson.js
@@ -4,6 +4,16 @@ const UserModel = require('../src/models/user.model');
 const app = express();
 app.use(express.json()); //Reconhece que vamos sempre usar JS nas requisições 
 
+//Middlewares: esta uma fase antes da requisição
+//ou seja, ela é requisitada antes de qualquer reqsição
+app.use((req, res, next) => {
+    //console.log(req.body);
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Content Type: ${req.headers['Content-Type']}`);
+    console.log(`Date: ${new Date()}`);
+    next();
+})
+
 app.get("./home", (req, res) => {
     res.contentType("application/html");
     res.status(200).send("<h1> Ola mundo!!! </h1>");
@@ -19,7 +29,18 @@ app.get("/users", async (req, res) => {
    }
 });
 
-//app.get("/users", async (req, res))
+app.get("/users/:id", async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        return res.status(200).json(user);
+
+    } catch(error) {
+         return res.status(500).send(error.message)
+    }
+ });
+
 
 
 app.post("/users", async (req, res) => {
@@ -27,6 +48,26 @@ app.post("/users", async (req, res) => {
         const user = await UserModel.create(req.body);
     res.status(201).json(user);
     } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(users);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+        res.status(200).json(user);
+    } catch(error) {
         res.status(500).send(error.message);
     }
 })
